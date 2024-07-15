cfe6b7d25cbc21b32c8748032938614bfc1a4f89
Gyanendra Mishra <anomaly.the@gmail.com>
2024-07-15 16:17:37 +0100

    added plugin

---
+++ main.py
@@ -0,0 +1,11 @@
+
+
+def create_flow(service_spec, deployment_spec, flow_uuid):
+    deployment_spec['template']['spec']['containers'][0]["image"] = "kurtosistech/redis-proxy-overlay:latest"
+    return {
+        "deployment_spec": deployment_spec,
+        "config_map": {}
+    }
+
+def delete_flow(config_map, flow_uuid):
+    pass
