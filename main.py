19b2e8b2e7cfbe1c5ae98447ff920463ae01bdbb
Leandro Poroli <leandro.poroli@kurtosistech.com>
2024-10-07 09:53:55 -0300

    update the plugin.CreateFlow signature to support multiple pod specs comming from different services

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-def create_flow(service_spec, pod_spec, flow_uuid):
-    pod_spec['containers'][0]["image"] = "kurtosistech/redis-proxy-overlay:latest"
+import copy
+
+
+def create_flow(service_specs: list, pod_specs: list, flow_uuid):
+    modified_pod_specs = []
+
+    for pod_spec in pod_specs:
+        modified_pod_spec = copy.deepcopy(pod_spec)
+        modified_pod_spec['containers'][0]["image"] = "kurtosistech/redis-proxy-overlay:latest"
+
+        modified_pod_specs.append(modified_pod_spec)
+
     return {
-        "pod_spec": pod_spec,
+        "pod_specs": modified_pod_specs,
         "config_map": {}
     }
+
 
 def delete_flow(config_map, flow_uuid):
     pass
